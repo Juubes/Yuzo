bb0a6901835874c9b82f473073c06198bd55e72f
Juubes <grumpt1@gmail.com>
2022-01-18 16:48:09 +0200

    config linting

--- functions/src/post/create-user.ts
+++ functions/src/post/create-user.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 
-let handler = functions
+const handler = functions
     .runWith({ memory: "128MB", maxInstances: 1, timeoutSeconds: 20 })
     .https.onRequest((req, res) => {
         if (req.method != "POST") return;
